da942eae2ef003e28dd6bc99696547ef57551a28
jmuchiri <mmjames451@gmail.com>
2025-07-10 22:48:39 +0300

    update: add new modify function to make changes to the virtual nodes

--- MiniMvc/Dom.js
+++ MiniMvc/Dom.js
@@ -29,3 +29,28 @@ export function apply(el, childrenDiff) {
     }
   });
 }
+
+function modify(el, diff) {
+  // Remove props
+  for (const prop of diff.remove) {
+    const event = eventName(prop);
+    if (event === null) {
+      el.removeAttribute(prop);
+    } else {
+      el._ui.listeners[event] = undefined;
+      el.removeEventListener(event, listener);
+    }
+  }
+
+  // Set props
+  for (const prop in diff.set) {
+    const value = diff.set[prop];
+    const event = eventName(prop);
+    event !== null
+      ? setListener(el, event, value)
+      : setProperty(prop, value, el);
+  }
+
+  // Deal with the children
+  apply(el, diff.children);
+}
