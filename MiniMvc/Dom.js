cf50f7febd3edeca01e304f363cbb6a039f8853d
jmuchiri <mmjames451@gmail.com>
2025-07-10 22:59:19 +0300

    feature: add new create function to create virtual nodes and there children( virtual nodes inside the above node )

--- MiniMvc/Dom.js
+++ MiniMvc/Dom.js
@@ -54,3 +54,33 @@ function modify(el, diff) {
   // Deal with the children
   apply(el, diff.children);
 }
+
+export function create(vnode) {
+  // Create a text node
+  if (vnode.text !== undefined) {
+    const el = document.createTextNode(vnode.text);
+    return el;
+  }
+
+  // Create the DOM element with the correct tag and
+  // already add our object of listeners to it.
+  const el = document.createElement(vnode.tag);
+  el._ui = { listeners: {} };
+
+  for (const prop in vnode.properties) {
+    const event = eventName(prop);
+    const value = vnode.properties[prop];
+    // If it's an event set it otherwise set the value as a property.
+    event !== null
+      ? setListener(el, event, value)
+      : setProperty(prop, value, el);
+  }
+
+  // Recursively create all the children and append one by one.
+  for (const childVNode of vnode.children) {
+    const child = create(childVNode);
+    el.appendChild(child);
+  }
+
+  return el;
+}
