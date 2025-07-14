7d02ec53b010bf372b3686a9430c4ce00d8fe85f
jkuya <kuyajesee@proton.me>
2025-07-14 23:50:59 +0300

    refactor

--- MiniMvc/event.js
+++ MiniMvc/event.js
@@ -1,29 +1,23 @@
-// browser event listerner which triggers our handler function stored in the event listerner
-//  each element has a map of handler for specific event listerners
 function listener(event) {
   const el = event.currentTarget;
   const handler = el._ui.listeners[event.type];
-  const enqueue = el_ui.enqueue;
+  const enqueue = el._ui.enqueue;
   const msg = handler(event);
   if (msg !== undefined) {
     enqueue(msg);
   }
 }
 
-// function to set up the vent listerner for the browser to listen
-// we also register specific function and thier corresponding functions
 export function setListener(el, event, handle) {
   if (el._ui.listeners[event] === undefined) {
     el.addEventListener(event, listener);
   }
-
   el._ui.listeners[event] = handle;
 }
 
 export function eventName(str) {
   if (str.indexOf("on") == 0) {
-    // starts with `on`
-    return str.slice(2).toLowerCase(); // lowercase name without the `on`
+    return str.slice(2).toLowerCase();
   }
   return null;
 }
