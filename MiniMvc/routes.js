a92245d6cd8bf078106589fd932c8b357fc4a9db
jkuya <kuyajesee@proton.me>
2025-07-17 00:10:03 +0300

    refactor: add documentation on how to add routes

--- MiniMvc/routes.js
+++ MiniMvc/routes.js
@@ -1,8 +1,24 @@
 //import the default functions from the files as shown in example
 // import Home from "./pages/home.js";
+//home.js should have something like this
+// export default (state) => ({
+//   tag: "div",
+//   attr: {},
+//   children: [
+//     { text: "Welcome to Home!" },
+//     {
+//       tag: "a",
+//       attr: { href: "/about", onclick: (e) => {
+//         e.preventDefault();
+//         window.history.pushState({}, "", "/about");
+//         state.enqueue({ type: "ROUTE_CHANGE", path: "/about" });
+//       }},
+//       children: [{ text: "Go to About" }]
+//     }
+//   ]
+// });
 
 // add the route inside the routes object as shown in the example
 // "/" : Home,
 export const routes = {
-
 }
