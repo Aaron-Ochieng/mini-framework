84831167d83ce3cd67cc3ada652d181d91e676cf
jkuya <kuyajesee@proton.me>
2025-07-17 00:07:39 +0300

    feat: define a router structure

---
+++ MiniMvc/routes.js
@@ -0,0 +1,8 @@
+//import the default functions from the files as shown in example
+// import Home from "./pages/home.js";
+
+// add the route inside the routes object as shown in the example
+// "/" : Home,
+export const routes = {
+
+}
