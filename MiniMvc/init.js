b3da1db8e26cf7c485bed4e3c7221698241e5d5e
jkuya <kuyajesee@proton.me>
2025-07-17 01:07:30 +0300

    feat: make the init function default

--- MiniMvc/init.js
+++ MiniMvc/init.js
@@ -1,10 +1,15 @@
 import { diffList } from "./diff.js";
 import { apply } from "./Dom.js";
 
+// root The real DOM element your app renders into. the div we target to inject code with
+// initialState (object) Defines initial values like todos, visibility filter, counters, etc The starting state of your application
+// update Type: (state, msg, enqueue?) => newState == A pure function that takes the current state and a message, then returns a new state.
+// view Type: (state) => VirtualNode[] A function that takes the current state and returns the virtual DOM tree.
+
 export default (root, initialState, update, view) => {
-  let state = initialState;
-  let nodes = [];
-  let queue = [];
+  let state = initialState; // client application state
+  let nodes = []; // virtual DOM nodes
+  let queue = []; // msg queue
 
   function enqueue(msg) {
     queue.push(msg);
